563053a24a937f96fbecfbe073ace3749d44ed8e
Piyushkr001 <piyushthegreat2305@gmail.com>
2025-12-18 14:41:48 +0530

    `Added placeholder module to lib/realtime-socket.ts`

--- lib/realtime-socket.ts
+++ lib/realtime-socket.ts
@@ -1,1 +1,9 @@
-realtime-socket.ts
+/**
+ * Placeholder module.
+ *
+ * Your app uses an external realtime server (see lib/realtime-emit.ts),
+ * so this file must be valid TypeScript to avoid build failures.
+ *
+ * If you later add an in-repo Socket.io server, implement it here.
+ */
+export {};
